5070dcf8bec0c81ca6c788e1fccf166057dc8688
TheHappyHacker <rob@thehappyhacker.net>
2024-08-14 20:50:34 -0400

    Update to include tertiary stats.

--- TrueStatValues.lua
+++ TrueStatValues.lua
@@ -89,7 +89,7 @@ local lee_cnv =    {
     6.883387706,	7.185293091,	7.514307661,	7.872890423,	8.263779433,	--   25
     8.690025176,	9.155028423,	9.662583199,	10.2169256,	    10.82278928,	--   30
     11.48546867,	12.21089087,	13.00569776,	13.87733962,	14.83418219,	--   35
-    15.88562903,	17.04226168,	18.31600031,	19.72028806,	21.2703027, 	--   40
+    15.88562903,	17.04226168,	18.31600031,	19.72028806,	21.2703027,	    --   40
     22.98320017,	24.87839486,	26.9778826,	    29.30661343,	31.89292206,	--   45
     34.76902578,	37.97160074,	41.54245007,	45.52927901,	49.98659542,	--   50
     54.97675724,	60.57119206,	66.85181896,	73.91270808,	81.86201984,	--   55
@@ -100,6 +100,44 @@ local lee_cnv =    {
     524.7914833,	586.2578415,	654.9234651,	731.6315702,	1019.995125,	--   80
   };
 
+local avd_cnv =   {
+    2.429933648,	2.429933648,	2.429933648,	2.429933648,	2.429933648,	--    5
+    2.429933648,	2.429933648,	2.429933648,	2.429933648,	2.429933648,	--   10
+    2.429933648,	2.55143033, 	2.672927012,	2.794423695,	2.915920377,	--   15
+    3.03741706,	    3.158913742,	3.280410424,	3.401907107,	3.523403789,	--   20
+    3.67114011,	    3.832156315,	4.007630753,	4.198874892,	4.407349031,	--   25
+    4.634680094,	4.882681826,	5.153377706,	5.449026985,	5.772154285,	--   30
+    6.125583292,	6.512475133,	6.936372139,	7.4012478,	    7.911563836,	--   35
+    8.47233548,	    9.089206227,	9.7685335,	    10.51748696,	11.34416144,	--   40
+    12.25770676,	13.26847726,	14.38820405,	15.63019383,	17.00955843,	--   45
+    18.54348041,	20.25152039,	22.15597337,	24.28228214,	26.65951756,	--   50
+    29.3209372,	    32.30463577,	35.65430345,	39.42011098,	43.65974391,	--   55
+    48.43961268,	53.83626993,	59.93807276,	66.84713441,	74.68161856,	--   60
+    78.88185118,	83.31831267,	88.00428898,	92.95381328,	98.18170799,	--   65
+    103.7036292,	109.5361135,	115.6966275,	122.2036202,	144.0038783,	--   70
+    160.8703752,	179.7123657,	200.7612298,	224.2754484,	250.5437769,	--   75
+    279.8887911,	312.6708488,	349.2925147,	390.2035041,	543.9974,	    --   80
+  };
+
+local spd_cnv = {
+    0.759354265,	0.759354265,	0.759354265,	0.759354265,	0.759354265,	--    5
+    0.759354265,	0.759354265,	0.759354265,	0.759354265,	0.759354265,	--   10
+    0.759354265,	0.797321978,	0.835289691,	0.873257405,	0.911225118,	--   15
+    0.949192831,	0.987160544,	1.025128258,	1.063095971,	1.101063684,	--   20
+    1.147231284,	1.197548848,	1.25238461, 	1.312148404,	1.377296572,	--   25
+    1.448337529,	1.52583807,	    1.610430533,	1.702820933,	1.803798214,	--   30
+    1.914244779,	2.035148479,	2.167616293,	2.312889937,	2.472363699,	--   35
+    2.647604838,	2.840376946,	3.052666719,	3.286714676,	3.54505045,     --   40
+    3.830533362,	4.146399143,	4.496313767,	4.884435572,	5.315487011,	--   45
+    5.794837629,	6.328600123,	6.923741679,	7.588213168,	8.331099237,	--   50
+    9.162792874,	10.09519868,	11.14196983,	12.31878468,	13.64366997,	--   55
+    15.13737896,	16.82383435,	18.73064774,	20.8897295,	    23.3380058,	    --   60
+    24.65057849,	26.03697271,	27.50134031,	29.04806665,	30.68178375,	--   65
+    32.40738412,	34.23003546,	36.15519609,	38.18863132,	45.00121196,	--   70
+    50.27199227,	56.16011427,	62.73788432,	70.08607764,	78.29493027,	--   75
+    87.46524721,	97.70964024,	109.1539108,	121.938595,	    169.9991875,	--   80
+  };
+
 function addon.tsv:SetupConversionFactors()
 	local level = UnitLevel("Player");
     level = math.max(level,1);
@@ -108,16 +146,44 @@ function addon.tsv:SetupConversionFactors()
 	addon.HasteConv 	= hst_cnv[level];
 	addon.VersConv 		= vrs_cnv[level];
 	addon.MasteryConv 	= mst_cnv[level];
-	--addon.LeechConv		= lee_cnv[level]*100;
+    addon.LeechConv     = lee_cnv[level];
+    addon.AvoidConv     = avd_cnv[level];
+    addon.SpeedConv     = spd_cnv[level];
 end
 
 local statIdMap = {
-    [CR_CRIT_SPELL]={["conversionFactor"]="CritConv",["rating"]="BaseCritRating"},
-    [CR_HASTE_SPELL]={["conversionFactor"]="HasteConv",["rating"]="BaseHasteRating"},
-    [CR_MASTERY]={["conversionFactor"]="MasteryConv",["rating"]="BaseMasteryRating"},
-    [CR_VERSATILITY_DAMAGE_DONE]={["conversionFactor"]="VersConv",["rating"]="BaseVersatilityRating"}
-}
+    [CR_CRIT_SPELL]={["conversionFactor"]="CritConv",["rating"]="BaseCritRating",["tertiary"]=false},
+    [CR_HASTE_SPELL]={["conversionFactor"]="HasteConv",["rating"]="BaseHasteRating",["tertiary"]=false},
+    [CR_MASTERY]={["conversionFactor"]="MasteryConv",["rating"]="BaseMasteryRating",["tertiary"]=false},
+    [CR_VERSATILITY_DAMAGE_DONE]={["conversionFactor"]="VersConv",["rating"]="BaseVersatilityRating",["tertiary"]=false},
+    [CR_LIFESTEAL]={["conversionFactor"]="LeechConv",["rating"]="BaseLeechRating",["tertiary"]=true},
+    [CR_AVOIDANCE]={["conversionFactor"]="AvoidConv",["rating"]="BaseAvoidanceRating",["tertiary"]=true},
+    [CR_SPEED]={["conversionFactor"]="SpeedConv",["rating"]="BaseSpeedRating",["tertiary"]=true},
+};
 
+
+function addon.tsv:SetupConversionFactors()
+	local level = UnitLevel("Player");
+    level = math.max(level,1);
+    
+	addon.CritConv 		= crt_cnv[level];
+	addon.HasteConv 	= hst_cnv[level];
+	addon.VersConv 		= vrs_cnv[level];
+	addon.MasteryConv 	= mst_cnv[level];
+	addon.LeechConv		= lee_cnv[level];
+	addon.AvoidConv		= avd_cnv[level];
+	addon.SpeedConv		= spd_cnv[level];
+end
+
+local statIdMap = {
+    [CR_CRIT_SPELL]={["conversionFactor"]="CritConv",["rating"]="BaseCritRating",["tertiary"]=false},
+    [CR_HASTE_SPELL]={["conversionFactor"]="HasteConv",["rating"]="BaseHasteRating",["tertiary"]=false},
+    [CR_MASTERY]={["conversionFactor"]="MasteryConv",["rating"]="BaseMasteryRating",["tertiary"]=false},
+    [CR_VERSATILITY_DAMAGE_DONE]={["conversionFactor"]="VersConv",["rating"]="BaseVersatilityRating",["tertiary"]=false},
+    [CR_LIFESTEAL]={["conversionFactor"]="LeechConv",["rating"]="BaseLeechRating",["tertiary"]=true},
+    [CR_AVOIDANCE]={["conversionFactor"]="AvoidConv",["rating"]="BaseAvoidanceRating",["tertiary"]=true},
+    [CR_SPEED]={["conversionFactor"]="SpeedConv",["rating"]="BaseSpeedRating",["tertiary"]=true},
+};
 
 --[[
     https://www.wowhead.com/news=318435/update-on-diminishing-returns-for-secondary-stats-in-shadowlands-new-thresholds-
@@ -132,14 +198,20 @@ local statIdMap = {
 ]]
 addon.tsv.StatBrackets = {
     {["size"]=30,["penalty"]=0},
-    {["size"]=9,["penalty"]=0.1},
-    {["size"]=8,["penalty"]=0.2},
-    {["size"]=7,["penalty"]=0.3},
-    {["size"]=12,["penalty"]=0.4},
-    {["size"]=60,["penalty"]=0.5},
-    {["size"]=60,["penalty"]=1.0},
+    {["size"]=10,["penalty"]=0.1},
+    {["size"]=10,["penalty"]=0.2},
+    {["size"]=10,["penalty"]=0.3},
+    {["size"]=20,["penalty"]=0.4},
+    {["size"]=120,["penalty"]=0.5},
     {["size"]=100000,["penalty"]=1.0},
-}
+};
+addon.tsv.TertiaryStatBrackets = {
+    {["size"]=10,["penalty"]=0},
+    {["size"]=5,["penalty"]=0.2},
+    {["size"]=5,["penalty"]=0.4},
+    {["size"]=80,["penalty"]=0.6},
+    {["size"]=100000,["penalty"]=1.0},
+};
 
 --penatly is the % penalty currently
 --trueRating is how much stat you effectively have after accounting for diminishes
@@ -161,13 +233,19 @@ function addon.tsv:GetStatDiminishBracket(statId,amount)
     local bracket_penalty = 0;
     local bracket_next_penalty = 0.1;
     local true_rating = 0;
+	local brackets;
+	if stat.tertiary then
+		brackets = self.TertiaryStatBrackets;
+	else
+		brackets = self.StatBrackets;
+	end
 
-    for i,bracket in ipairs(self.StatBrackets) do
+    for i,bracket in ipairs(brackets) do
         if ( percent < bracket.size ) then
             bracket_rating = math.floor(0.5+percent * conversionFactor);
             bracket_max_rating = math.floor(0.5+bracket.size * conversionFactor);
             bracket_penalty = bracket.penalty;
-            bracket_next_penalty = self.StatBrackets[i+1] and self.StatBrackets[i+1].penalty or 1;
+            bracket_next_penalty = brackets[i+1] and brackets[i+1].penalty or 1;
             true_rating = true_rating + (percent * conversionFactor * (1.0 - bracket.penalty) );
             break;
         else
@@ -186,6 +264,9 @@ function addon.tsv:RecalculateTrueStatRatings()
     addon.BaseHasteRating = GetCombatRating(CR_HASTE_SPELL);
     addon.BaseMasteryRating = GetCombatRating(CR_MASTERY);
     addon.BaseVersatilityRating = GetCombatRating(CR_VERSATILITY_DAMAGE_DONE);
+    addon.BaseLeechRating = GetCombatRating(CR_LIFESTEAL);
+    addon.BaseSpeedRating = GetCombatRating(CR_SPEED);
+    addon.BaseAvoidanceRating = GetCombatRating(CR_AVOIDANCE);
 
     addon.TrueStatInfo = addon.TrueStatInfo or {};
     for statId,stat in pairs(statIdMap) do 
